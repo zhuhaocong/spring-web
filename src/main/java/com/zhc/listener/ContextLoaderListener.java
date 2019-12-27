65db8ce4c2e22dd2c3a0c55981141f1676213ef2
zhuhaocong <zhuhaocong@git.com>
2019-12-27 15:03:03 +0800

    spring集成web

--- src/main/java/com/zhc/listener/ContextLoaderListener.java
+++ src/main/java/com/zhc/listener/ContextLoaderListener.java
@@ -11,10 +11,14 @@ import javax.servlet.ServletContextListener;
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-
+        //获得servletContext对象
         ServletContext servletContext = sce.getServletContext();
-        //读取web.xml中的配置
+        //读取web.xml中配置的配置文件
         String contextConfigLocation = servletContext.getInitParameter("contextConfigLocation");
+
+        System.out.println(contextConfigLocation);
+
+        //创建spring容器
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext(contextConfigLocation);
         servletContext.setAttribute("applicationContext",applicationContext);
     }
